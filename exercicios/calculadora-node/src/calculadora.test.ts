2bf4b0aaf5ed278f2561c68248c58243b8b195d5
unknown <23214290046@academico.iesb.br>
2025-08-29 21:39:08 -0300

    jest

---
+++ exercicios/calculadora-node/src/calculadora.test.ts
@@ -0,0 +1,29 @@
+// import cod test
+
+import * as cal from "./calculadora";
+
+// importo as funcionalidades do jest
+import { describe, expect, test } from "@jest/globals";
+
+// describe para fazer o agrupamento dos testes
+describe("Testando Módulo Calculadora", () => {
+  // construcao dos testes unitarios
+  test("calcular soma: 3 + 11 = 14", () => {
+    expect(cal.somar(3, 11)).toBe(14);
+  });
+  test("calcular subtracao: 10 - 5 = 5", () => {
+    expect(cal.subtrair(10, 5)).toBe(5);
+  });
+  test("calcular multiplicacao: 5 * 10 = 50", () => {
+    expect(cal.multiplicar(5, 10)).toBe(50);
+  });
+  test("calcualr divisao: 30 / 6 = 5", () => {
+    expect(cal.dividir(30, 6)).toBe(5);
+  });
+  test("calcular potenciacao: 5 ** 2 = 25", () => {
+    expect(cal.aoQuadrado(5)).toBe(25);
+  });
+  test("calcular raiz quadrada: 112 por volta de 10.6", () => {
+    expect(cal.raizQuadrada(112)).toBeCloseTo(10.6, 2);
+  });
+});
